263c20c5f2b60189c5678eead50f2989fa6ae79c
takuizum <sep10.taku.izum@gmail.com>
2019-04-14 00:58:02 +0900

    GIRT simulation start

--- base.R
+++ base.R
@@ -5,3 +5,9 @@
 # 2, Unstability of phi parameters estimati
 
 library(irtfun2)
+library(tidyverse)
+
+res <- estGip(sim_dat_girt, fc = 2, Nphi = 21, esteap = T, min_ph = 0.001, max_ph = 4, phi_dist = "gbeta")
+res$item
+
+res$person$phi %>% hist()
